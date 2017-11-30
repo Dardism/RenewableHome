1390a3d6668463c4599838c849a62171f39042ab
Michael <dardismm@gmail.com>
2017-11-30 12:02:04 -0500

    updated views, added in sliders for energy selection. updated images for selectors

--- src/RenewableHome/scripts/custom.js
+++ src/RenewableHome/scripts/custom.js
@@ -1,3 +1,30 @@
 ﻿$('#drpState').change(function () {
   $('#txtKWperMonth').val(this.value).css('border-color', 'green').css('border-width', '3px');
-  })
+})
+
+
+//slider 
+$("#sliderSolar").slider();
+$("#sliderSolar").on("slide", function (slideEvt) {
+  $("#sliderSolarSliderVal").text(slideEvt.value);
+});
+
+$("#sliderWind").slider();
+$("#sliderWind").on("slide", function (slideEvt) {
+  $("#sliderWindSliderVal").text(slideEvt.value);
+});
+
+$("#sliderHydro").slider();
+$("#sliderHydro").on("slide", function (slideEvt) {
+  $("#sliderHydroSliderVal").text(slideEvt.value);
+});
+
+$("#sliderGeo").slider();
+$("#sliderGeo").on("slide", function (slideEvt) {
+  $("#sliderGeoSliderVal").text(slideEvt.value);
+});
+
+$("#sliderNuclear").slider();
+$("#sliderNuclear").on("slide", function (slideEvt) {
+  $("#sliderNuclearSliderVal").text(slideEvt.value);
+});
